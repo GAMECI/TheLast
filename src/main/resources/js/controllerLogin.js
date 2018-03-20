31560c23934868ffa872740ce5c9d360d6e41922
danielDaleman <danieldaleman@hotmail.com>
2018-03-20 11:53:58 -0500

    Agregar identificador de partida

--- src/main/resources/js/controllerLogin.js
+++ src/main/resources/js/controllerLogin.js
@@ -1,9 +1,10 @@
 function dateLogin(ctrl){	
 	var name = document.getElementById("idName").value;
+	var game = document.getElementById("idGame").value;
     for(i=0;i<ctrl.length;i++){
         if(ctrl[i].checked){
 			var color = ctrl[i].value;
 		}
 	}
-	alert('{ Nombre: ' + name + ' , ' + 'Color: ' + color +' }');
+	alert('{ Nombre: ' + name + ' , ' + 'Color: ' + color + ' , ' + 'IdGame: ' + game + ' }');
 }
